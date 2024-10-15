93ffc8e162d1ef293e8586a5582aee866419e474
samuelbozio <samuelbozio@hotmail.com>
2024-10-15 13:20:08 -0300

    add3

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
             { 
                 duration: 2, 
                 y: 0, 
-                rotation: 2, 
+                rotation: 1.5, 
                 opacity: 1, 
                 ease: "power2.out",
                 onComplete: function() {
